8dcba4643dc7fb0f69c75a4d177dc87196e98a04
Paridax <tgavdaly@gmail.com>
2022-11-27 21:16:48 -0500

    Completed first release test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate piston_window;
+extern crate graphics;
+extern crate rand;
 
 mod pixel;
 
@@ -11,8 +13,8 @@ fn main() {
     // make a new window called Pixel Physics
     let window: PistonWindow =
         WindowSettings::new("Pixel Physics", [(WIDTH * SCALE_FACTOR) as u32, (HEIGHT * SCALE_FACTOR) as u32])
-            .resizable(false).build().unwrap();
-    let window = window.ups(10);
+            .resizable(true).build().unwrap();
+    let window = window.ups(1);
     let mut window = window.exit_on_esc(true);
 
     let (mut mouse_x, mut mouse_y) = (0, 0);
@@ -65,7 +67,8 @@ fn main() {
         // if the mouse is pressed, draw a pixel
         if draw {
             if pixel_at(&world, mouse_x, mouse_y).0 != brush_type as u8 {
-                create_pixel(&mut world, brush_type, cycle, density(brush_type), mouse_x, mouse_y);
+                // create_pixel(&mut world, brush_type, cycle, density(brush_type), mouse_x, mouse_y);
+                create_pixel_rect(&mut world, brush_type, cycle, density(brush_type), mouse_x - 5, mouse_y - 5, 11, 11);
             }
         }
 
@@ -74,103 +77,169 @@ fn main() {
             let pixel = world[i as usize];
             // if the pixel has already been moved this cycle, skip it
             // if the pixel is air or rock, it will never move
-            if get_vector(pixel) == [0, 0] { // if the x and y vector are 0
-                continue;
-            } else if match_cycle(pixel, cycle) || match_type(pixel, AIR) || match_type(pixel, ROCK) {
-                continue;
-            } else {
-                // if the pixel is sand, it will move down if there is air below it
-                if match_type(pixel, SAND) {
-                    let (x, y) = loc(i);
-                    if compare_density(pixel, pixel_at(&world, x, y - 1)) {
-                        // set x vector to 0
-                        world[i as usize].3[0] = 0;
-                        swap_pixels(&mut world, x, y, x, y - 1, cycle);
-                    } else {
-                        // if there is no air below it, it will move down and to the left or right if there is air there
-                        if compare_density(pixel, pixel_at(&world, x - 1, y - 1)) {
-                            // set x vector to -1
-                            world[i as usize].3[0] = -1;
-                            swap_pixels(&mut world, x, y, x - 1, y - 1, cycle);
-                        } else if compare_density(pixel, pixel_at(&world, x + 1, y - 1)) {
-                            // set x vector to 1
-                            world[i as usize].3[0] = 1;
-                            swap_pixels(&mut world, x, y, x + 1, y - 1, cycle);
-                        }
-                    }
+            if match_cycle(pixel, cycle) || match_type(pixel, AIR) || match_type(pixel, ROCK) {
+                // reset the update flag
+                if ! match_type(pixel, AIR) {
+                    world[i as usize].4 = true;
                 }
-
-                if match_type(pixel, WATER) {
-                    let (x, y) = loc(i);
-                    if compare_density(pixel, pixel_at(&world, x, y - 1)) {
-                        world[i as usize].3[0] = 0;
-                        swap_pixels(&mut world, x, y, x, y - 1, cycle);
-                    } else {
-                        // if there is no air below it, it will move down and to the left or right if there is air there
-                        if compare_density(pixel, pixel_at(&world, x - 1, y - 1)) {
-                            world[i as usize].3[0] = -1;
-                            swap_pixels(&mut world, x, y, x - 1, y - 1, cycle);
-                        } else if compare_density(pixel, pixel_at(&world, x + 1, y - 1)) {
-                            world[i as usize].3[0] = 1;
-                            swap_pixels(&mut world, x, y, x + 1, y - 1, cycle);
-                        } else {
-                            if get_vector_x(pixel) == 0 {
-                                // pick randomly true or false
-                                let rand = rand::random::<bool>();
-                                if rand {
-                                    world[i as usize].3[0] = 1;
-                                } else {
-                                    world[i as usize].3[0] = -1;
-                                }
-                            } else if get_vector_x(pixel) == 1 {
-                                if compare_density(pixel, pixel_at(&world, x + 1, y)) {
-                                    swap_pixels(&mut world, x, y, x + 1, y, cycle);
-                                } else {
-                                    world[i as usize].3[0] = 0;
-                                }
-                            } else if get_vector_x(pixel) == -1 {
-                                if compare_density(pixel, pixel_at(&world, x - 1, y)) {
-                                    swap_pixels(&mut world, x, y, x - 1, y, cycle);
-                                } else {
-                                    world[i as usize].3[0] = 0;
-                                }
-                            }
-                        }
-                    }
-                }
+                continue;
+            } else /* if get_vector_y(pixel) != 0 */ {
+                update_pixel(&mut world, i, cycle);
             }
         }
 
         // render pixels on screen
         window.draw_2d(&event, |context, graphics, _device| {
-            // clear screen
-            // clear([1.0; 4], graphics);
+
+            // display_world(&world, 0, world.len() as u64);
+            clear([0.0; 4], graphics);
 
             // loop over each pixel
-            for i in (0..world.len() as u64).rev() {
+            for i in (0..world.len() as u64) {
                 // if the pixel is air then skip drawing it
-                if get_update_state(world[i as usize]) == false {
-                    continue
-                } else {
-                    update_state(&mut world, i as usize, false);
-                    let pixel = world[i as usize];
+                let pixel = world[i as usize];
+                if ! match_type(pixel, AIR) {
+                    // let (x, y) = loc(i);
+                    // let mut col = color(get_type(pixel));
+                    // col[3] = 0.1;
+                    // rectangle(col,
+                    //           // square at x, y with width of the pixel size
+                    //           draw_pixel(x, y),
+                    //           context.transform,
+                    //           graphics);
+                }
+                if world[i as usize].4 {
+                    // println!("{}", world[i as usize].4);
                     let (x, y) = loc(i);
 
-                    rectangle(color(get_type(pixel)),
+                    let chunk_size = find_chunk_size(&world, x, y);
+
+                    update_pixels_in_chunk(&mut world, x, y, chunk_size);
+                    // display_world(&world, 0, world.len() as u64);
+                    let mut col = color(get_type(pixel));
+                    if DEBUG {
+                        if get_type(pixel) == WATER {
+                            col = [(x as f32)/(WIDTH as f32 - 1.0),(y as f32)/(HEIGHT as f32-1.0), 1.0, 0.1];
+                        } else {
+                            col = [(x as f32)/(WIDTH as f32 - 1.0),(y as f32)/(HEIGHT as f32-1.0), 0.0, 1.0]
+                        }
+                    }
+                    rectangle(col,
                               // square at x, y with width of the pixel size
-                              draw_pixel(x, y),
+                              draw_chunk(x, y, chunk_size),
                               context.transform,
                               graphics);
                 }
             }
 
             // draw mouse cursor
-            // rectangle([1.0, 1.0, 1.0, 1.0],
-            //           // square at x, y with width of the pixel size
-            //           draw_pixel(mouse_x, mouse_y),
-            //           context.transform,
-            //           graphics);
-        });
+            rectangle([1.0, 1.0, 1.0, 0.3],
+                      // square at x, y with width of the pixel size
+                      draw_pixel(mouse_x, mouse_y),
+                      context.transform,
+                      graphics);
+            }
+        );
+    }
+}
+
+fn update_pixel(mut world: &mut[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize],
+                array_pos: u64, cycle: i32) {
+    // if we are updating a pixel that doesnt exist, return
+    if array_pos < 0 || array_pos >= world.len() as u64 {
+        return;
+    }
+    let pixel = world[array_pos as usize];
+    // if the pixel has already been updated this cycle, return
+    if get_cycle(pixel) == cycle {
+        return;
+    }
+    // if the pixel is air or rock, return
+    if match_type(pixel, AIR) || match_type(pixel, ROCK) {
+        return;
+    }
+
+    if match_type(pixel, SAND) {
+        let (x, y) = loc(array_pos);
+        if compare_density(pixel, pixel_at(&world, x, y - 1)) {
+            // set x vector to 0
+            world[array_pos as usize].3[0] = 0;
+            swap_pixels(&mut world, x, y, x, y - 1, cycle);
+        } else {
+            // if there is no air below it, it will move down and to the left or right if there is air there
+            if compare_density(pixel, pixel_at(&world, x - 1, y - 1)) {
+                // set x vector to -1
+                world[array_pos as usize].3[0] = -1;
+                swap_pixels(&mut world, x, y, x - 1, y - 1, cycle);
+            } else if compare_density(pixel, pixel_at(&world, x + 1, y - 1)) {
+                // set x vector to 1
+                world[array_pos as usize].3[0] = 1;
+                swap_pixels(&mut world, x, y, x + 1, y - 1, cycle);
+            } else {
+                world[array_pos as usize].3[0] = 0;
+                world[array_pos as usize].3[1] = 0;
+            }
+        }
+    }
+
+    if match_type(pixel, WATER) {
+        let (x, y) = loc(array_pos);
+        if compare_density(pixel, pixel_at(&world, x, y - 1)) {
+            world[array_pos as usize].3[0] = 0;
+            // if the pixel below is water, use rng to decide if it should move left or right or stay still
+            if match_type(pixel_at(&world, x, y - 1), WATER) {
+                // random bool
+                let rand = rand::random::<bool>();
+                if rand {
+                    // update cycle
+                    world[array_pos as usize].1 = cycle as u8;
+                    return;
+                } else {
+                    swap_pixels(&mut world, x, y, x, y - 1, cycle);
+                }
+            } else {
+                swap_pixels(&mut world, x, y, x, y - 1, cycle);
+            }
+        } else {
+            if compare_density(pixel, pixel_at(&world, x - 1, y - 1)) {
+                // down and to the left
+                world[array_pos as usize].3[0] = -1;
+                swap_pixels(&mut world, x, y, x - 1, y - 1, cycle);
+            } else if compare_density(pixel, pixel_at(&world, x + 1, y - 1)) {
+                // down and to the right
+                world[array_pos as usize].3[0] = 1;
+                swap_pixels(&mut world, x, y, x + 1, y - 1, cycle);
+            } else {
+                if get_vector_x(pixel) == 0 {
+                    // pick randomly true or false which direction to move
+                    let rand = rand::random::<bool>();
+                    if rand {
+                        world[array_pos as usize].3[0] = 1;
+                    } else {
+                        world[array_pos as usize].3[0] = -1;
+                    }
+                } else if get_vector_x(pixel) == 1 {
+                    // move to the right if we already have right movement
+                    if compare_density(pixel, pixel_at(&world, x + 1, y)) {
+                        swap_pixels(&mut world, x, y, x + 1, y, cycle);
+                    } else {
+                        // swap directions if we cant move right
+                        world[array_pos as usize].3[0] = -1;
+                    }
+                } else if get_vector_x(pixel) == -1 {
+                    // move to the left if we already have left movement
+                    if compare_density(pixel, pixel_at(&world, x - 1, y)) {
+                        swap_pixels(&mut world, x, y, x - 1, y, cycle);
+                    } else {
+                        // swap directions if we cant move left
+                        world[array_pos as usize].3[0] = 1;
+                    }
+                } else {
+                    world[array_pos as usize].3[0] = 0;
+                    world[array_pos as usize].3[1] = 0;
+                }
+            }
+        }
     }
 }
 
@@ -195,11 +264,11 @@ fn density(pixel_type: i32) -> i32 {
 }
 
 //<editor-fold desc="DEBUG">
-fn display_world(world: [(u8, u8, u8, [u32; 2]); (WIDTH * HEIGHT) as usize], start: u64, end: u64) {
+fn display_world(world: &[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize], start: u64, end: u64) {
     for i in (start..end).rev() {
-        let (t, cycle, density, position) = world[i as usize];
+        let (t, cycle, density, position, display_update) = world[i as usize];
         let (pos_x, pos_y) = loc(i);
-        println!("Type: {0}, Cycle: {1}, Density: {2}, Vector: ({3}, {4}), Pos: ({5}, {6})", t, cycle, density, position[0], position[1], pos_x, pos_y);
+        println!("Type: {0}, Cycle: {1}, Density: {2}, Vector: ({3}, {4}), Pos: ({5}, {6}), Update Sc: {7}", t, cycle, density, position[0], position[1], pos_x, pos_y, display_update);
     }
 }
 //</editor-fold>
--- src/pixel.rs
+++ src/pixel.rs
@@ -1,5 +1,13 @@
+pub const DEBUG: bool = false;
+
 pub const WIDTH: i32 = 256;
-pub const HEIGHT: i32 = 256;
+pub const HEIGHT: i32 = WIDTH;
+
+pub const MAX_CHUNK_SIZE: i32 = WIDTH;
+const SQUARE_CHUNK_SCAN: bool = true;
+const RECT_SCAN_X_BIAS: bool = false;
+const CHUNK_COARSENESS: i32 = 8;
+
 pub const SCALE_FACTOR: i32 = 3;
 
 pub const AIR: i32 = 0;
@@ -14,8 +22,8 @@ pub const D_WATER: i32 = 1;
 
 pub const COL_AIR: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 pub const COL_ROCK: [f32; 4] = [0.4, 0.4, 0.4, 1.0];
-pub const COL_SAND: [f32; 4] = [1.0, 0.53, 0.0, 1.0];
-pub const COL_WATER: [f32; 4] = [0.0, 0.25, 1.0, 1.0];
+pub const COL_SAND: [f32; 4] = [1.0, 0.6, 0.0, 1.0];
+pub const COL_WATER: [f32; 4] = [0.0, 0.4, 1.0, 1.0];
 
 const X: usize = 0;
 const Y: usize = 1;
@@ -27,11 +35,28 @@ pub fn create_pixel(world: &mut[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) a
     if t == WATER || t == SAND {
         vector = [0, -1];
     }
-    return world[coord(x, y)] = (t as u8, cycle as u8, dens as u8, vector, true);
+    if x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT {
+        // if the pixel is already this type, don't change it
+        if ! match_type(world[coord(x, y)], t) {
+            world[coord(x, y)] = (t as u8, cycle as u8, dens as u8, vector, true);
+        }
+    }
+}
+
+pub fn create_pixel_rect(world: &mut[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize],
+                    t: i32, cycle: i32, dens: i32, x: i32, y: i32, width: i32, height: i32) {
+    for i in 0..width {
+        for j in 0..height {
+            create_pixel(world, t, cycle, dens, x + i, y + j);
+        }
+    }
 }
 
 pub fn draw_pixel(x: i32, y: i32) -> [f64; 4] {
-    return [(x * SCALE_FACTOR) as f64, ((HEIGHT - y - 1) * SCALE_FACTOR) as f64, SCALE_FACTOR as f64, SCALE_FACTOR as f64];
+    if DEBUG {
+        return [(x * SCALE_FACTOR + 1) as f64 , ((HEIGHT - y - 1) * SCALE_FACTOR + 1) as f64, SCALE_FACTOR as f64 - 1.0, SCALE_FACTOR as f64 - 1.0];
+    }
+    return [(x * SCALE_FACTOR) as f64 , ((HEIGHT - y - 1) * SCALE_FACTOR) as f64, SCALE_FACTOR as f64, SCALE_FACTOR as f64];
 }
 
 // turn x, y into array loc
@@ -52,7 +77,7 @@ pub fn loc(array_pos: u64) -> (i32, i32) {
 pub fn pixel_at(world: &[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize], x: i32, y: i32) -> (u8, u8, u8, [i32; 2], bool) {
     // if the pixel is out of bounds, return air
     if x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT {
-        return (ROCK as u8, 0, 3, [0, 0], false);
+        return (10 as u8, 0, 3, [0, 0], false);
     }
     return world[coord(x, y)];
 }
@@ -73,17 +98,17 @@ pub fn get_density(pixel: (u8, u8, u8, [i32; 2], bool)) -> i32 {
     return pixel.2 as i32;
 }
 
-pub fn get_vector(pixel: (u8, u8, u8, [i32; 2], bool)) -> [i32; 2] {
-    return [pixel.3[X] as i32, pixel.3[Y] as i32];
-}
+// pub fn get_vector(pixel: (u8, u8, u8, [i32; 2], bool)) -> [i32; 2] {
+//     return [pixel.3[X] as i32, pixel.3[Y] as i32];
+// }
 
 pub fn get_vector_x(pixel: (u8, u8, u8, [i32; 2], bool)) -> i32 {
     return pixel.3[X] as i32;
 }
 
-pub fn get_vector_y(pixel: (u8, u8, u8, [i32; 2], bool)) -> i32 {
-    return pixel.3[Y] as i32;
-}
+// pub fn get_vector_y(pixel: (u8, u8, u8, [i32; 2], bool)) -> i32 {
+//     return pixel.3[Y] as i32;
+// }
 
 // pixel comparison
 pub fn match_type(pixel: (u8, u8, u8, [i32; 2], bool), t: i32) -> bool {
@@ -101,6 +126,7 @@ pub fn compare_density(pixel: (u8, u8, u8, [i32; 2], bool), d: (u8, u8, u8, [i32
 pub fn swap_pixels(world: &mut[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize], x1: i32, y1: i32, x2: i32, y2: i32, cycle: i32) {
     // update the cycle
     world[coord(x1, y1)].1 = cycle as u8;
+    world[coord(x2, y2)].1 = cycle as u8;
 
     // tell renderer to redraw the pixels
     world[coord(x1, y1)].4 = true;
@@ -114,4 +140,111 @@ pub fn swap_pixels(world: &mut[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as
 
 pub fn update_state(world: &mut[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize], arr_index: usize, state: bool) {
     world[arr_index].4 = state;
+}
+
+
+pub fn find_chunk_size(world: &[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize], x: i32, y: i32) -> (u32, u32) {
+    // check if the pixel is already updated
+    if !get_update_state(pixel_at(world, x, y)) {
+        return (0, 0);
+    }
+    let t = get_type(pixel_at(world, x, y));
+
+    let mut blocked_x = false;
+    let mut blocked_y = false;
+
+    // scan larger and larger squares up and to the right of the first pixel until we find a pixel that isn't the same type
+    // as the first pixel
+    let mut size = 1;
+    for i in 1..MAX_CHUNK_SIZE {
+        let mut found = false;
+        for j in 0..i {
+            if get_type(pixel_at(world, x + j, y + i)) != t as i32 || pixel_at(world, x + j, y + i).4 == false {
+                found = true;
+                blocked_y = true;
+                break;
+            }
+        }
+        for j in 0..i {
+            if get_type(pixel_at(world, x + i, y + j)) != t as i32 || pixel_at(world, x + i, y + j).4 == false {
+                found = true;
+                blocked_x = true;
+                break;
+            }
+        }
+        if get_type(pixel_at(world, x + i, y + i)) != t as i32 || pixel_at(world, x + i, y + i).4 == false {
+            if RECT_SCAN_X_BIAS {
+                blocked_x = true;
+            } else {
+                blocked_y = true;
+            }
+            found = true;
+        }
+        if found {
+            break;
+        }
+        size += 1;
+    }
+    if SQUARE_CHUNK_SCAN {
+        return (size, size);
+    } else {
+        let mut size_x = size;
+        let mut size_y = size;
+
+        if blocked_x {
+            // if the rectangle is not tall enough, it isn't worth it to make a really skinny rect
+            // because it is more likely a more efficient rect can be found further up the scan
+            if size_x < CHUNK_COARSENESS as u32 {
+                return(size_x, size_y);
+            }
+            // keep increasing the y size until we find a pixel that isn't the same type as the first pixel
+            for i in size_y as i32..MAX_CHUNK_SIZE {
+                let mut found = false;
+                for j in 0..size as i32 {
+                    if get_type(pixel_at(world, x + j, y + i)) != t as i32 || pixel_at(world, x + j, y + i).4 == false {
+                        found = true;
+                        break;
+                    }
+                }
+                if found {
+                    break;
+                }
+                size_y += 1;
+            }
+        } else if blocked_y {
+            // same as above, but for the y size
+            if size_y < CHUNK_COARSENESS as u32 {
+                return(size_x, size_y);
+            }
+            // keep increasing the x size until we find a pixel that isn't the same type as the first pixel
+            for i in size_x as i32..MAX_CHUNK_SIZE {
+                let mut found = false;
+                for j in 0..size as i32 {
+                    if get_type(pixel_at(world, x + i, y + j)) != t as i32 || pixel_at(world, x + i, y + j).4 == false {
+                        found = true;
+                        break;
+                    }
+                }
+                if found {
+                    break;
+                }
+                size_x += 1;
+            }
+        }
+
+        return (size_x, size_y);
+    }
+}
+
+pub fn update_pixels_in_chunk(world: &mut[(u8, u8, u8, [i32; 2], bool); (WIDTH * HEIGHT) as usize], x: i32, y: i32, chunk_size: (u32, u32)) {
+    // update the pixels in the area
+    for i in 0..chunk_size.0 as i32 {
+        for j in 0..chunk_size.1 as i32 {
+            update_state(world, coord(x + i, y + j), false);
+        }
+    }
+}
+
+pub fn draw_chunk(x: i32, y: i32, chunk_size: (u32, u32)) -> [f64; 4] {
+    return [(x * SCALE_FACTOR) as f64, ((HEIGHT - y - chunk_size.1 as i32) * SCALE_FACTOR) as f64, (chunk_size.0 as i32 * SCALE_FACTOR) as f64, (chunk_size.1 as i32 * SCALE_FACTOR) as f64];
 }
